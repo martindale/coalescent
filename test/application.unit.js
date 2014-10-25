3e16a22c527ff2c46ee512b00297ae96e52f3f4c
Gordon Hall <gordon@gordonwritescode.com>
2014-10-25 16:19:42 -0400

    add application unit tests

--- test/application.unit.js
+++ test/application.unit.js
@@ -1,6 +1,13 @@
-var should   = require('should');
-var sinon    = require('sinon');
-var coalesce = require('..');
+var should  = require('should');
+var sinon   = require('sinon');
+var coal    = require('..');
+var net     = require('net');
+var stream  = require('stream');
+var through = require('through');
+
+var stublog = {
+  info: sinon.stub(), warn: sinon.stub(), error: sinon.stub()
+};
 
 before(function(done) {
   done();
@@ -12,130 +19,189 @@ after(function(done) {
 
 describe('Application', function() {
 
+  var app1, app2, app3;
+
   describe('@constructor', function() {
 
-    it.skip('should create a proper instance', function(done) {
-
+    it('should create a proper instance', function(done) {
+      app1 = coal({ logger: stublog });
+      app1.server.should.be.instanceOf(net.Server);
+      app1.stack.should.have.lengthOf(1);
+      should.exist(app1.pipe);
+      should.exist(app1.write);
+      should.exist(app1.pipe);
+      done();
     });
 
   });
 
   describe('#use', function() {
 
-    it.skip('should register the middleware', function(done) {
+    it('should register the middleware', function(done) {
+      var middleware = function(socket) {
+        socket._processed = true;
+        return through(function(data) { this.queue(data); });
+      };
 
+      middleware._plugin = function(app) {
+        app.beep = 'boop';
+      };
+
+      app1.use(middleware);
+      app1.stack.should.have.lengthOf(2);
+      done();
+    });
+
+    it('should apply the _plugin on registration', function(done) {
+      app1.beep.should.equal('boop');
+      done();
     });
 
   });
 
   describe('#set', function() {
 
-    it.skip('should set the value to the key and return it', function(done) {
-
+    it('should set the value to the key and return it', function(done) {
+      app1.set('beep', 'boop');
+      app1.options.beep.should.equal('boop');
+      done();
     });
 
   });
 
   describe('#get', function() {
 
-    it.skip('return the non-null value', function(done) {
-
+    it('return the non-null value', function(done) {
+      app1.get('beep').should.equal('boop');
+      done();
     });
 
-    it.skip('return null for non-existent key', function(done) {
-
+    it('return null for non-existent key', function(done) {
+      var check = app1.get('boop');
+      should.not.exist(check);
+      done();
     });
 
   });
 
   describe('#listen', function() {
 
-    it.skip('should begin accepting connections on the port', function(done) {
-
+    it('should begin accepting connections on the port', function(done) {
+      app1.listen(1337, function() {
+        var sock = net.connect(1337, function() {
+          sock._connecting.should.equal(false);
+          done();
+        });
+      });
     });
 
   });
 
   describe('#connect', function() {
 
-    it.skip('should open a connection to the target', function(done) {
-
-    });
-
-  });
-
-  describe('#close', function() {
-
-    it.skip('should stop accepting connections', function(done) {
-
+    it('should open a connection to the target', function(done) {
+      app2 = coal({ logger: stublog });
+      var conn = app2.connect(1337, function() {
+        conn._connecting.should.equal(false);
+        done();
+      });
     });
 
   });
 
   describe('#peers', function() {
 
-    it.skip('should return an array of all peers', function(done) {
-
+    it('should return an array of all peers', function(done) {
+      var peers = app1.peers();
+      peers.should.have.lengthOf(2);
+      done();
     });
 
-    it.skip('should call the function for each peer', function(done) {
+    it('should call the function for each peer', function(done) {
+      var total  = app1.peers().length;
+      var called = 0;
+      app1.peers(function(p) {
+        if (p instanceof net.Socket) called++;
+      });
+      should(total).equal(called);
+      done();
+    });
 
+  });
+
+  describe('#write', function() {
+
+    it('should write message to peers', function(done) {
+      app2.once('data', function(d) {
+        d.toString().should.equal('beep boop\n');
+        done();
+      });
+      app1.write('beep boop\n');
     });
 
   });
 
   describe('#_enterNetwork', function() {
 
-    it.skip('should open connections to supplied seeds', function(done) {
-
+    it('should open connections to supplied seeds', function(done) {
+      app2.listen(1338, function() {
+        app3 = coal({
+          logger: stublog,
+          seeds: ['127.0.0.1:1337','127.0.0.1:1338']
+        });
+        app3.connections.outbound.should.have.lengthOf(2);
+        done();
+      });
     });
 
   });
 
   describe('#_process', function() {
 
-    it.skip('should pipe the given socket through the stack', function(done) {
-
+    it('should pipe the given socket through the stack', function(done) {
+      app1.peers(function(p) {
+        should.exist(p._processed);
+      });
+      done();
     });
 
   });
 
   describe('#_initMiddlewareStack', function() {
 
-    it.skip('should return a new instance of the stack', function(done) {
-
+    it('should return a new instance of the stack', function(done) {
+      var sock  = new net.Socket();
+      var stack = app1._initMiddlewareStack(sock);
+      stack.should.have.lengthOf(app1.stack.length);
+      stack.should.not.equal(app1.stack);
+      should.exist(sock._processed);
+      done();
     });
 
   });
 
   describe('#_cleanup', function() {
 
-    it.skip('should remove the socket from the peer list', function(done) {
-
-    });
-
-  });
-
-  describe('#_read', function() {
-
-    it.skip('should do nothing', function(done) {
-
-    });
-
-  });
-
-  describe('#_write', function() {
-
-    it.skip('should write message to all peers', function(done) {
-
+    it('should remove the socket from the peer list', function(done) {
+      var sock = new net.Socket();
+      var len  = app1.connections.inbound.length;
+      app1.connections.inbound.push(sock);
+      app1.connections.inbound.should.lengthOf(len + 1);
+      app1._cleanup(sock);
+      app1.connections.inbound.should.lengthOf(len);
+      done();
     });
 
   });
 
   describe('#_handleInbound', function() {
 
-    it.skip('should add the socket to the inbound peer list', function(done) {
-
+    it('should add the socket to the inbound peer list', function(done) {
+      var sock = new net.Socket();
+      var len  = app2.connections.inbound.length;
+      app2._handleInbound(sock);
+      app2.connections.inbound.should.have.lengthOf(len + 1);
+      done();
     });
 
   });
