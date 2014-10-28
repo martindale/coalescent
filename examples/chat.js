f050b3941423f4cf10d34575cb24d9e6e1a472fc
Gordon Hall <gordon@gordonwritescode.com>
2014-10-28 15:51:24 -0400

    fix _enterNetwork(), add better error handling, move old examples out since they are now integration tests, add obligatory chat app, bupm version

---
+++ examples/chat.js
@@ -0,0 +1,59 @@
+/*
+** An Obligatory Chat App
+*/
+
+var program    = require('commander');
+var read       = require('read');
+var rl         = require('readline');
+var color      = require('cli-color');
+var coalescent = require('..');
+
+program
+  .version('0.0.0')
+  .option('-s, --seeds <host:port,host:port...>', 'seeds for entering network')
+  .option('-p, --port <port>', 'listen on port', 1337)
+  .parse(process.argv);
+
+var app = coalescent({
+  seeds: program.seeds ? program.seeds.split() : [],
+  logger: false
+});
+
+app.use(coalescent.tattletale());
+app.use(coalescent.courier());
+app.use(coalescent.router());
+
+read({ prompt: 'Handle: ', default: 'Anonymous' }, function(err, handle) {
+
+  var iface = rl.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  iface.on('line', function(data) {
+    app.broadcast('chat', { handle: handle, text: data.toString() });
+    iface.prompt(true);
+  }).resume();
+
+  app.route('chat', function(socket, message) {
+    socket.handle = message.body.handle;
+    console.log(color.bold(message.body.handle + ': ') + message.body.text);
+    iface.prompt(true);
+  });
+
+  app.on('peerConnected', function(peer) {
+    console.log(color.italic((peer.handle || 'Someone') + ' joined!'));
+    iface.prompt(true);
+  });
+
+  app.on('peerDisconnected', function(peer) {
+    console.log(color.italic((peer.handle || 'Someone') + ' left!'));
+    iface.prompt(true);
+  });
+
+  console.log('Welcome! Type a message and press <return> to chat.\n');
+
+  iface.prompt(true);
+
+  app.listen(program.port);
+});
