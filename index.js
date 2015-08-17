a311416d17c1dd143975ea5024900709ddde7fcc
Gordon Hall <gordon@gordonwritescode.com>
2015-08-17 00:26:01 -0400

    cleanup, jshint compliance

--- index.js
+++ index.js
@@ -1,19 +1,10 @@
-var Application = require('./lib/application');
-var courier     = require('./lib/middleware/courier');
-var router      = require('./lib/middleware/router');
-var tattletale  = require('./lib/middleware/tattletale');
-var smartrelay  = require('./lib/middleware/smartrelay');
+'use strict';
 
-// expose app constructor
-function coalescent(opts) {
-  return new Application(opts);
-};
+var coalescent = require('./lib/application');
 
-// expose middleware
-coalescent.courier    = courier;
-coalescent.router     = router;
-coalescent.tattletale = tattletale;
-coalescent.smartrelay = smartrelay;
+coalescent.courier = require('./lib/middleware/courier');
+coalescent.router = require('./lib/middleware/router');
+coalescent.tattletale = require('./lib/middleware/tattletale');
+coalescent.smartrelay = require('./lib/middleware/smartrelay');
 
-// expose module
 module.exports = coalescent;
